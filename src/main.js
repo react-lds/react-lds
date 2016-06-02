0a4fb22ce3fe03468b22828be191204e36478732
Felix Spöttel <hi@felics.me>
2016-06-02 15:38:23 +0200

    Add utility functions
     - Add utility function to prepend the cssPrefix
     - Add utility function to render modifiers
     - Use 'flavor'-string instead of booleans
     - Add PropType to validate 'flavor'-string
     - Rewrite MediaObject to use flavors & pass figures as prop

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-import { MediaObject, MediaObjectBody, MediaObjectFigure } from './components/MediaObject';
+import MediaObject from './components/MediaObject';
 import PageHeader from './components/PageHeader';
 import Icon from './components/Icon';
 
 export {
   Icon,
   MediaObject,
-  MediaObjectBody,
-  MediaObjectFigure,
   PageHeader,
 };
