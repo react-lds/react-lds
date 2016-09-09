19c9b3762020d279e7d14042444f41dd427e588d
Felix Spöttel <hi@felics.me>
2016-09-09 17:23:35 +0200

    Fix Travis Token

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -3,9 +3,9 @@
 set -o errexit -o nounset
 
 # Do nothing if the commit was not pushed to master
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$TRAVIS_BRANCH" != "master" ]
 then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  echo "This commit was either a pull request or made against the $TRAVIS_BRANCH and not the master! No deploy!"
   exit 0
 fi
 
@@ -20,12 +20,9 @@ npm run build:docs
 rev=$(git rev-parse --short HEAD)
 cd ./docs/build
 git init
-git remote add upstream "https://$GH_TOKEN@github.com/propertybase/react-lds.git"
-git fetch upstream
-git reset upstream/gh-pages
 
 # Add all changes in push them to gh-pages
 touch .
 git add -A .
 git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
