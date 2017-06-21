ba520f078c6a40a820637231029107fd1973652f
Siamend <simo@propertybase.com>
2017-06-21 15:40:33 +0200

    Update examples to show both sections

--- docs/src/app/pages/ExpandableSection/ExampleDefault.js
+++ docs/src/app/pages/ExpandableSection/ExampleDefault.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 import { ExpandableSection } from 'react-lds';
 
-const section = {
-  id: 'a',
-  content: <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-              do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-              ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              Duis aute irure dolor in reprehenderit in voluptate velit
-              esse cillum dolore eu fugiat nulla pariatur. Excepteur
-              sint occaecat cupidatat non proident, sunt
-              in culpa qui officia deserunt mollit anim id est laborum.</p>
-};
-
-const ExampleDefault = () => <ExpandableSection open title="Test Title" section={section} />;
+const ExampleDefault = () =>
+(
+  <ExpandableSection open id="123" title="Lorem Ipsum">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna
+      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit
+      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+      sint occaecat cupidatat non proident, sunt
+      in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </ExpandableSection>
+);
 
 export default ExampleDefault;
