2e6319f34b6ee68782b17e76fd0f9e5daa93282e
Philipp Alexander Frank <philipp.alexander.frank@gmail.com>
2017-06-21 10:56:52 +0200

    Refactoring with simo

--- src/components/ExpandableSection/ExpandableSection.js
+++ src/components/ExpandableSection/ExpandableSection.js
@@ -1,80 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 import { Button, ButtonIcon } from '../../';
 
-class ExpandableSection extends React.Component {
-
+class ExpandableSection extends Component {
   constructor(props) {
-    super();
+    super(props);
+
+    const { open } = props;
+
     this.state = {
-      contentId: props.section.id,
-      open: props.open
+      open,
     };
-  //  this.onClickToggle = this.onClickToggle.bind(this);
-  }
-  // props destructen .. in eine const schreiben
-
-  onClickToggle() {
-    this.setState({ open: !this.state.open });
   }
 
-  isUncollapsable() {
-    return this.props.uncollapsable ?
-    (
-      <span
-        className="slds-truncate slds-p-horizontal_small"
-        title={this.props.title}
-      >
-        {this.props.title}
+  onClickToggle = () => {
+    this.setState(state => ({ open: !state.open }));
+  }
+
+  renderUncollapsable() {
+    const { title } = this.props;
+
+    return (
+      <span className="slds-truncate slds-p-horizontal_small" title={title}>
+        {title}
       </span>
-    )
-    :
-    (
+    );
+  }
+
+  renderCollapsable() {
+    const { id, title } = this.props;
+    const { open } = this.state;
+
+    return (
       <Button
         icon
-        aria-controls={this.props.section.id}
-        aria-expanded={this.state.open.toString()}
+        aria-controls={id}
+        aria-expanded={open.toString()}
         className="slds-section__title-action"
-        onClick={() => this.onClickToggle()}
+        onClick={this.onClickToggle}
       >
         <ButtonIcon
           position="left"
-          icon={this.state.open ? 'chevrondown' : 'chevronright'}
+          icon={open ? 'chevrondown' : 'chevronright'}
           sprite="utility"
           aria-hidden="true"
         />
-        <span className="slds-truncate" title={this.props.title}>
-          {this.props.title}
+        <span className="slds-truncate" title={title}>
+          {title}
         </span>
       </Button>
-
     );
   }
+
   render() {
-  //  const className = this.props;
+    const { children, className, id, uncollapsable } = this.props;
+    const { open } = this.state;
+
     const sldsClasses = [
       'slds-section',
-      { 'slds-is-open': !!this.state.open },
+      { 'slds-is-open': open },
+      className,
     ];
 
     const headerClasses = [
       'slds-section__title',
-      { 'slds-theme_shade': !!this.props.uncollapsable },
+      { 'slds-theme_shade': uncollapsable },
     ];
-    console.log((this.state.open).toString());
+
     return (
       <div className={cx(sldsClasses)}>
         <h3 className={cx(headerClasses)}>
-          {this.isUncollapsable()}
+          {uncollapsable
+            ? this.renderUncollapsable()
+            : this.renderCollapsable()}
         </h3>
-        <div
-          aria-hidden={(!this.state.open).toString()}
-          className="slds-section__content"
-          id={this.props.section.id}
-        >
-          {this.props.section.content}
+        <div aria-hidden={(!open).toString()} className="slds-section__content" id={id}>
+          {children}
         </div>
       </div>
     );
@@ -83,7 +86,6 @@ class ExpandableSection extends React.Component {
 
 ExpandableSection.defaultProps = {
   className: null,
-  section: null,
   title: null,
   open: true,
   uncollapsable: false,
@@ -102,11 +104,7 @@ ExpandableSection.propTypes = {
   /**
    * id is required (to link content to button), content can be empty
    */
-  section: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    content: PropTypes.node,
-  }
-  ).isRequired,
+  id: PropTypes.string.isRequired,
   /**
    * defines whether section is expandable or not
    */
@@ -119,5 +117,7 @@ ExpandableSection.propTypes = {
    * triggered when the user clicks the expand button
    */
   onClickToggle: PropTypes.func,
+
+  children: PropTypes.node,
 };
 export default ExpandableSection;
