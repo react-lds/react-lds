888ee951f9dabb0eb83b4c5031897a0465846c73
Siamend <simo@propertybase.com>
2017-06-21 10:33:18 +0200

    work in progress

---
+++ docs/src/app/pages/ExpandableSection/ExampleNoncollapsable.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { ExpandableSection } from 'react-lds';
+
+const section = {
+  id: 'a',
+  content: <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+              do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Duis aute irure dolor in reprehenderit in voluptate velit
+              esse cillum dolore eu fugiat nulla pariatur. Excepteur
+              sint occaecat cupidatat non proident, sunt
+              in culpa qui officia deserunt mollit anim id est laborum.</p>
+};
+
+const ExampleNoncollapsable = () => <ExpandableSection uncollapsable open title="Test Title" section={section} />;
+
+export default ExampleNoncollapsable;
