417ccb5158fa0517715a626b0d204f836323e21a
Bruno <bruno.pejac@gmail.com>
2017-06-27 19:59:11 +0200

    update Spinner component with container

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -5,7 +5,7 @@ import cx from 'classnames';
 import { flavorable } from '../../decorators';
 
 export const Spinner = (props) => {
-  const { className, size, ...rest } = props;
+  const { className, size, container, fixed, ...rest } = props;
 
   const sldsClasses = [
     'slds-spinner',
@@ -13,14 +13,28 @@ export const Spinner = (props) => {
     className
   ];
 
-  return (
-    <div className="slds-spinner_container">
-      <div {...rest} className={cx(sldsClasses)} aria-hidden="false" role="alert">
+  const renderSpinner = () =>
+    (
+      <div {...rest} className={cx(sldsClasses)} role="status">
+        <span className="slds-assistive-text">Loading</span>
         <div className="slds-spinner__dot-a" />
         <div className="slds-spinner__dot-b" />
       </div>
-    </div>
-  );
+    );
+
+  if (container) {
+    const containerClasses = [
+      'slds-spinner_container',
+      { 'slds-is-fixed': fixed },
+    ];
+    return (
+      <div className={cx(containerClasses)}>
+        {renderSpinner()}
+      </div>
+    );
+  }
+
+  return renderSpinner();
 };
 
 Spinner.flavors = [
@@ -31,15 +45,16 @@ Spinner.flavors = [
 Spinner.defaultProps = {
   className: null,
   size: 'medium',
+  container: false,
+  fixed: false,
 };
 
 Spinner.propTypes = {
 
   className: PropTypes.string,
-
-
   size: PropTypes.oneOf(['xx-small', 'x-small', 'small', 'medium', 'large']),
-
+  container: PropTypes.bool,
+  fixed: PropTypes.bool,
 };
 
 export default flavorable(Spinner, 'spinner');
