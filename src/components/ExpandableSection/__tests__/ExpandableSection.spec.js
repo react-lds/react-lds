800823cee89d9082e04cbec13166f39ab3bdcff8
Siamend <simo@propertybase.com>
2017-07-07 16:56:02 +0200

    Update tests to correspond to changes in component

--- src/components/ExpandableSection/__tests__/ExpandableSection.spec.js
+++ src/components/ExpandableSection/__tests__/ExpandableSection.spec.js
@@ -9,25 +9,25 @@ const getExpandableSection = (props = {}) => shallow(<ExpandableSection id="51er
 
 describe('<ExpandableSection />', () => {
   it('renders right class name', () => {
-    const mounted = getExpandableSection();
-    mounted.setProps({ open: true });
+    const mounted = getExpandableSection({ defaultOpen: true });
     expect(mounted.hasClass('slds-section')).toBeTruthy();
   });
 
   it('renders children', () => {
     const child = <div className="51ers" />;
-    const mounted = getExpandableSection({ id: 'test', children: child });
-    mounted.setProps({ open: true });
+    const mounted = getExpandableSection({ id: 'test', children: child, open: true });
     expect(mounted.contains(child)).toBeTruthy();
   });
 
   it('renders a title', () => {
-    const mounted = getExpandableSection();
-    mounted.setProps({ title: 'Title', open: false });
+    const mounted = getExpandableSection({
+      title: 'Title',
+      open: false,
+      uncollapsable: false
+    });
     expect(mounted.find(Button).find('span').prop('title')).toEqual('Title');
     expect(mounted.find(Button).find('span').text()).toEqual('Title');
   });
-
   it('renders right slds-classes when open is set to true', () => {
     const mounted = getExpandableSection({ open: true });
     expect(mounted.find('.slds-section').hasClass('slds-is-open')).toBeTruthy();
@@ -36,18 +36,22 @@ describe('<ExpandableSection />', () => {
     const mounted = getExpandableSection({ open: false });
     expect(mounted.find('.slds-section').hasClass('slds-is-open')).toBeFalsy();
   });
-  it('does render right class when collapsable is false', () => {
-    const mounted = getExpandableSection({ collapsable: false });
+  it('does render right class when uncollapsable is true', () => {
+    const mounted = getExpandableSection({ uncollapsable: true, open: false });
     expect(mounted.find('.slds-truncate').hasClass('slds-p-horizontal_small')).toBeTruthy();
   });
   it('renders right slds-classes when defaultOpen is set to true', () => {
-    const mounted = getExpandableSection();
-    mounted.setProps({ defaultOpen: true });
+    const mounted = getExpandableSection({ defaultOpen: true });
     expect(mounted.find('.slds-section').hasClass('slds-is-open')).toBeTruthy();
   });
-  it('renders right slds-classes when defaultOpen is set to null', () => {
-    const mounted = getExpandableSection();
-    mounted.setProps({ defaultOpen: null, open: true });
+  it('renders right slds-classes when defaultOpen is set to null & open to true', () => {
+    const mounted = getExpandableSection({ open: true });
     expect(mounted.find('.slds-section').hasClass('slds-is-open')).toBeTruthy();
+  });
+  it('renders component right in controlled mode (open: true -> open: false -> open: true)', () => {
+    const mounted = getExpandableSection({ open: true });
+    expect(mounted.find('.slds-section').hasClass('slds-is-open')).toBeTruthy();
+    mounted.setProps({ open: false });
+    expect(mounted.find('.slds-section').hasClass('slds-is-open')).toBeFalsy();
   });
 });
