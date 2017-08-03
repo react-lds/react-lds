502d15b3257414f76d6dc6a296652f6bf8d30d05
Bruno <bruno.pejac@gmail.com>
2017-08-03 09:35:08 +0200

    Added title attr to Avatar

--- docs/src/app/pages/Images/ExampleNormal.js
+++ docs/src/app/pages/Images/ExampleNormal.js
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 import { Avatar } from 'react-lds';
 
 const ExampleNormal = (props, { assetBasePath }) =>
-  <Avatar alt="Normal Image" src={`${assetBasePath}assets/images/avatar2.jpg`} size="medium" />;
+  <Avatar
+    alt="Normal Image"
+    src={`${assetBasePath}assets/images/avatar2.jpg`}
+    size="medium"
+    title="Mr Nobody avatar"
+  />;
 
 ExampleNormal.contextTypes = {
   assetBasePath: PropTypes.string,
