b6b166e5af3eb02d14d722306bc4ff436837b03c
Bruno <bruno.pejac@gmail.com>
2017-09-18 14:10:19 +0200

    Add FieldLevelHelp

---
+++ src/components/Form/FieldLevelHelp.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+import { Button, ButtonIcon } from '../../';
+
+const FieldLevelHelp = (props) => {
+  const {
+    onClick,
+    className,
+    describedId,
+    tooltip,
+    ...rest,
+  } = props;
+
+  const sldsClasses = [
+    'slds-form-element__icon',
+    className,
+  ];
+
+  return (
+    <div className={cx(sldsClasses)} {...rest}>
+      <Button icon onClick={onClick} tooltip={tooltip} >
+        <ButtonIcon sprite="utility" icon="info" />
+      </Button>
+    </div>
+  );
+};
+
+FieldLevelHelp.propTypes = {
+  /*
+   * Function for Tooltip Button
+   */
+  onClick: PropTypes.func,
+  /*
+   * class name
+   */
+  className: PropTypes.string,
+  /*
+   * described by (id)
+   */
+  describedId: PropTypes.string,
+  /*
+   * tooltip title
+   */
+  title: PropTypes.string,
+  /*
+   * tooltipHelp tooltip
+   */
+  tooltip: PropTypes.string,
+};
+
+FieldLevelHelp.defaultProps = {
+  onClick: null,
+  className: null,
+  describedId: null,
+  title: null,
+  tooltip: null,
+};
+
+export default FieldLevelHelp;
