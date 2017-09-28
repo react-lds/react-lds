b5c8e7c37d6b92ac301897354f4c777b2a6364a8
Felix Spöttel <hi@felics.me>
2017-09-28 12:05:48 +0200

    Move decorators to global storybook scope

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,7 @@
-import { configure } from '@storybook/react';
-import { setDefaults } from '@storybook/addon-info';
+import { addDecorator, configure } from '@storybook/react';
+import { setDefaults, withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withContext, withSpacing } from '../stories/utils/helpers';
 
 // addon-info
 setDefaults({
@@ -19,5 +21,10 @@ const req = require.context('../stories', true, /.js$/);
 function loadStories() {
   req.keys().forEach((filename) => req(filename));
 }
+
+addDecorator((story, context) => withInfo('Usage')(story)(context));
+addDecorator(withContext);
+addDecorator(withSpacing);
+addDecorator(withKnobs);
 
 configure(loadStories, module);
--- stories/Badge.js
+++ stories/Badge.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, text } from '@storybook/addon-knobs';
-import { withInfo } from '@storybook/addon-info';
-import { getThemes, withPadding } from './utils/helpers';
+import { text } from '@storybook/addon-knobs';
+import { getThemes } from './utils/helpers';
 import { Badge } from '../src';
 
 const stories = storiesOf('Badge', module);
 
 stories
-  .addDecorator(withPadding)
-  .addDecorator(withKnobs);
-
-const renderDefault = () => (
-  <Badge
-    onClick={action('click')}
-    label={text('Label', 'Sample Badge')}
-    theme={getThemes()}
-  />
-);
-
-stories.add('Default', withInfo()(renderDefault));
+  .add('Default', () => (
+    <Badge
+      onClick={action('click')}
+      label={text('Label', 'Sample Badge')}
+      theme={getThemes()}
+    />
+  ));
