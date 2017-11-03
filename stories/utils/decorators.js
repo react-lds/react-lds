04e2f42b7198a042ac2148f5a9ca634b97a8907b
Bruno <bruno.pejac@gmail.com>
2017-11-03 18:01:58 +0100

    Add some Decorators and Utils

--- stories/utils/decorators.js
+++ stories/utils/decorators.js
@@ -11,3 +11,20 @@ export const SpinnerDecorator = storyFn => (
     { storyFn() }
   </div>
 );
+
+export const PromptDecorator = storyFn => (
+  <div className="demo-modal">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br />
+    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit<br />
+    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum.
+    { storyFn() }
+  </div>
+);
+
+export const NotificationDecorator = storyFn => (
+  <div className="demo-notifications">
+    { storyFn() }
+  </div>
+);
