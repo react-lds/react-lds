ea83ad561bc875b61c80b11ec2511eff57e4870b
Bruno Péjac <brunottonurb@users.noreply.github.com>
2017-12-06 22:57:25 +0100

    Update changelog and bump version (#227)
    * Update changelog and bump version
    * Fix auto-deployment

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -9,5 +9,20 @@ then
   exit 0
 fi
 
-# Deploy with storybook-deployer, user is Travis (specified in package.json)
-yarn deploy-storybook
+#  Set Travis as commiter git user
+git config --global user.email "propertybase-ci@propertybase.com"
+git config --global user.name "Propertybase CI"
+
+# Build docs
+yarn build-storybook
+
+# Initialize an empty repository, fetch upstream and reset it
+rev=$(git rev-parse --short HEAD)
+cd ./react-lds
+git init
+
+# Add all changes in push them to gh-pages
+touch .
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
