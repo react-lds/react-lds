9c5c733e688a1cd0e3ff23afd48c246e7155d719
extronics <post@calitrix.de>
2017-12-12 09:13:30 +0100

    Revert to previous export

--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -1,5 +1,5 @@
-import ObjectHome from './ObjectHome';
+import ObjectHome, { ObjectHomeRaw } from './ObjectHome';
 import PageHeaderBase from './PageHeaderBase';
 import RecordHome from './RecordHome';
 
-export { ObjectHome, PageHeaderBase, RecordHome };
+export { ObjectHome, ObjectHomeRaw, PageHeaderBase, RecordHome };
