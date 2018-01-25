8fd13036fc6d11bfd77eb32f155a03f88c1672c0
Felix Spöttel <hi@felics.me>
2018-01-25 11:00:12 +0100

    Add example for SpinnerContainer

--- stories/Spinner.js
+++ stories/Spinner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Spinner } from '../src';
 import { boolean, select } from '@storybook/addon-knobs';
+import { Spinner, SpinnerContainer } from '../src';
 import { getSizes } from './utils/helpers';
 import { SpinnerDecorator } from './utils/decorators';
 
@@ -15,4 +15,12 @@ stories
       flavor={select('Flavor', ['', 'brand', 'inverse'], '') || undefined}
       size={getSizes()}
     />
+  ))
+  .add('With Container', () => (
+    <SpinnerContainer fixed={boolean('Fixed', false)}>
+      <Spinner
+        flavor={select('Flavor', ['', 'brand', 'inverse'], '') || undefined}
+        size={getSizes()}
+      />
+    </SpinnerContainer>
   ));
