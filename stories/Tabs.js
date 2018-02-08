eb531c01e21b9a9bde1e999d6c3b36556f7ef21e
Felix Spöttel <hi@felics.me>
2018-02-08 11:59:34 +0100

    Wrap controlled example with info

--- stories/Tabs.js
+++ stories/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 import { action } from '@storybook/addon-actions';
 import { boolean, select } from '@storybook/addon-knobs';
 import { ControlledTabs, Tabs, Tab } from '../src';
@@ -23,7 +24,9 @@ stories
       <Tab id="tab-3" title="Tab 3">Tab Three</Tab>
     </Tabs>
   ))
-  .add('Controlled', () => (
+  .add('Controlled', withInfo(`
+    **ControlledTabs** are the underlying implementation of **Tabs**. They receive the active tab id via the prop _activeTab_ and hand off changes via _onChangeTab_. Note that this demo does not implement any event handling and focus styles will appear off because of this.
+  `)(() => (
     <ControlledTabs
       onChangeTab={action('change-tab')}
       activeTab={select('Tab Open', ['tab-1', 'tab-2', 'tab-3'], 'tab-2')}
@@ -36,4 +39,4 @@ stories
       </Tab>
       <Tab id="tab-3" title="Tab 3">Tab Three</Tab>
     </ControlledTabs>
-  ));
+  )));
