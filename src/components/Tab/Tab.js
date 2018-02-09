98d4bc9f3ea42827c74de5c5426bbd2d5caf2dfe
Felix Spöttel <fspoettel@users.noreply.github.com>
2018-02-09 18:31:22 +0100

    Revert "New Tabs Implementation (#273)"
    This reverts commit ebae69cb44ecda08b66eae184bc73e874052fae7.

---
+++ src/components/Tab/Tab.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { omit } from 'lodash';
+
+// https://github.com/oliviertassinari/babel-plugin-transform-react-remove-prop-types#is-it-safe
+const propTypes = {
+  /**
+   * class name
+   */
+  className: PropTypes.string,
+  /**
+   * array of tabs
+   */
+  tabs: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    content: PropTypes.node.isRequired,
+  })).isRequired,
+  /**
+   * scoped has a border around the tab
+   */
+  scoped: PropTypes.bool,
+  /**
+   * Renders the tabs in card look
+   */
+  styled: PropTypes.bool,
+};
+
+class Tab extends Component {
+  static defaultProps = {
+    className: null,
+    scoped: false,
+    styled: false,
+  }
+
+  static propTypes = propTypes
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = { activeTab: this.props.tabs[0].id };
+  }
+
+  setActiveTab(id) {
+    this.setState({ activeTab: id });
+  }
+
+  renderHeader() {
+    const { tabs, scoped } = this.props;
+    const { activeTab } = this.state;
+
+    return tabs.map((tab, index) => {
+      const boundClick = this.setActiveTab.bind(this, tab.id);
+      const headerClasses = [
+        `slds-tabs_${scoped ? 'scoped' : 'default'}__item`,
+        'slds-text-title_caps',
+        { 'slds-active': activeTab === tab.id },
+      ];
+
+      return (
+        <li
+          key={tab.id}
+          className={cx(headerClasses)}
+          title={tab.title}
+          role="presentation"
+          onClick={boundClick}
+        >
+          <a
+            className={`slds-tabs_${scoped ? 'scoped' : 'default'}__link`}
+            role="tab"
+            tabIndex={index}
+            aria-selected={activeTab === tab.id}
+            aria-controls={tab.id}
+            id={`${tab.id}__item`}
+          >
+            {tab.title}
+          </a>
+        </li>
+      );
+    });
+  }
+
+  renderBody() {
+    const { tabs, scoped } = this.props;
+    const { activeTab } = this.state;
+
+    return tabs.map((tab) => {
+      const bodyClasses = [
+        `slds-tabs_${scoped ? 'scoped' : 'default'}__content`,
+        { 'slds-show': activeTab === tab.id },
+        { 'slds-hide': activeTab !== tab.id },
+      ];
+
+      return (
+        <div
+          key={tab.id}
+          id={tab.id}
+          className={cx(bodyClasses)}
+          role="tabpanel"
+          aria-labelledby={`${tab.id}__item`}
+        >
+          {tab.content}
+        </div>
+      );
+    });
+  }
+
+  render() {
+    const { className, scoped, styled } = this.props;
+    const rest = omit(this.props, Object.keys(propTypes));
+
+    const sldsClasses = [
+      `slds-tabs_${scoped ? 'scoped' : 'default'}`,
+      { 'slds-tabs_card': styled },
+      className
+    ];
+
+    return (
+      <div {...rest} className={cx(sldsClasses)}>
+        <ul className={`slds-tabs_${scoped ? 'scoped' : 'default'}__nav`} role="tablist">
+          {this.renderHeader()}
+        </ul>
+        {this.renderBody()}
+      </div>
+    );
+  }
+}
+
+export default Tab;
--- src/components/Tabs/utils.js
+++
@@ -1,7 +0,0 @@
-export const getTabsClass = (str, scoped = false) => {
-  const postfix = str || '';
-  const modifier = scoped ? 'scoped' : 'default';
-  return `slds-tabs_${modifier}${postfix}`;
-};
-
-export const getAriaLabel = id => `${id}__item`;
