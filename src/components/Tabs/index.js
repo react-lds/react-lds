277236a767bf17b2bf97d62fed3679c6468a96de
Felix Spöttel <fspoettel@users.noreply.github.com>
2018-02-13 10:19:38 +0100

    Revert "New Tabs Implementation (#273)" (#282)
    This reverts commit ebae69cb44ecda08b66eae184bc73e874052fae7.

--- src/components/Tabs/index.js
+++
@@ -1,5 +0,0 @@
-import ControlledTabs from './ControlledTabs';
-import Tabs from './Tabs';
-import Tab from './Tab';
-
-export { ControlledTabs, Tab, Tabs };
--- stories/Tabs.js
+++ stories/Tabs.js
@@ -1,42 +1,33 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import { action } from '@storybook/addon-actions';
-import { boolean, select } from '@storybook/addon-knobs';
-import { ControlledTabs, Tabs, Tab } from '../src';
+import { boolean } from '@storybook/addon-knobs';
+import { Tab } from '../src';
 
 const stories = storiesOf('Tabs', module);
 
+const tabs = [
+  {
+    title: 'Tab One',
+    id: 'tab-1',
+    content: <p>I am the first tab!</p>,
+  },
+  {
+    title: 'Tab Two',
+    id: 'tab-2',
+    content: <p>I am the second tab!</p>,
+  },
+  {
+    title: 'Tab Three',
+    id: 'tab-3',
+    content: <p>I am the third tab!</p>,
+  },
+];
+
 stories
   .add('Default', () => (
-    <Tabs
-      styled={boolean('Card Look?', true)}
-      scoped={boolean('Scoped?', false)}
-      onChangeTab={action('change-tab')}
-      renderInactiveTabs={boolean('Render Inactive Tabs (Inspect to see change)', false)}
-    >
-      <Tab id="tab-1" title="Tab 1">Tab One</Tab>
-      <Tab id="tab-2" title="Tab 2">
-        <span>
-          Tab Two with <button>a focusable element.</button>
-        </span>
-      </Tab>
-      <Tab id="tab-3" title="Tab 3">Tab Three</Tab>
-    </Tabs>
-  ))
-  .add('Controlled', withInfo(`
-    **ControlledTabs** are the underlying implementation of **Tabs**. They receive the active tab id via the prop _activeTab_ and hand off changes via _onChangeTab_. Note that this demo does not implement any event handling and focus styles will appear off because of this.
-  `)(() => (
-    <ControlledTabs
-      onChangeTab={action('change-tab')}
-      activeTab={select('Tab Open', ['tab-1', 'tab-2', 'tab-3'], 'tab-2')}
-    >
-      <Tab id="tab-1" title="Tab 1">Tab One</Tab>
-      <Tab id="tab-2" title="Tab 2">
-        <span>
-          Tab Two with <button>a focusable element.</button>
-        </span>
-      </Tab>
-      <Tab id="tab-3" title="Tab 3">Tab Three</Tab>
-    </ControlledTabs>
-  )));
+    <Tab
+      tabs={tabs}
+      scoped={boolean('Scoped', false)}
+      styled={boolean('Card-Look', false)}
+    />
+  ));
