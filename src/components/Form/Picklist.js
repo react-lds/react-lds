dc5f018a881c90388d6acf95624739065e94d1e2
extronics <post@calitrix.de>
2018-03-16 11:09:24 +0100

    Import from lodash/memoize

--- src/components/Form/Picklist.js
+++ src/components/Form/Picklist.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import enhanceWithClickOutside from 'react-click-outside';
-import { memoize } from 'lodash';
+import memoize from 'lodash/memoize';
 
 import {
   PicklistDropdown,
