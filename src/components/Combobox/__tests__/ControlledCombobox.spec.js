412b0e2c3392a2b6d1a72d3474b386ab39ca5b0d
extronics <post@calitrix.de>
2018-05-02 17:42:11 +0200

    Fix test

--- src/components/Combobox/__tests__/ControlledCombobox.spec.js
+++ src/components/Combobox/__tests__/ControlledCombobox.spec.js
@@ -3,7 +3,7 @@ import { mount } from 'enzyme';
 
 import Combobox from '../ControlledCombobox';
 
-describe('</Combobox />', () => {
+describe('</ControlledCombobox />', () => {
   let mounted = null;
   let props = {};
   let onSelect = jest.fn();
@@ -68,11 +68,18 @@ describe('</Combobox />', () => {
     expect(mounted.find('ul.slds-listbox').hasClass('slds-dropdown_length-with-icon-5')).toBeTruthy();
   });
 
-  it('is closed by default and opens when the input was clicked', () => {
+  it('is closed by default and opens when the input was clicked', (done) => {
     const picklist = mounted.find('div.slds-combobox').first();
     expect(picklist.hasClass('slds-is-open')).toBeFalsy();
     picklist.find('input').simulate('click');
-    expect(picklist.hasClass('slds-is-open')).toBeTruthy();
+
+    setTimeout(() => {
+      try {
+        expect(picklist.hasClass('slds-is-open')).toBeTruthy();
+      } finally {
+        done();
+      }
+    }, 0);
   });
 
   it('sets the label of the currently selected item as input label', () => {
