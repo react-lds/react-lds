6bb7f36db51f4f534d2237e14ddbc27fd4a31e92
Felix Spöttel <hi@felics.me>
2018-05-16 13:10:33 +0200

    Refactor File, add specs

---
+++ src/components/File/FileMedia.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Icon } from '../..';
+
+const FileMedia = ({
+  fileType,
+  image,
+  isLoading,
+  title,
+}) => {
+  if (isLoading) {
+    return (
+      <Icon
+        className="slds-file__icon"
+        sprite="utility"
+        icon="image"
+        size="large"
+        svgClassName="slds-file__loading-icon"
+        title={title}
+      />
+    );
+  }
+
+  if (image && image.src) return <img src={image.src} alt={image.alt || title} />;
+
+  return (
+    <Icon
+      className="slds-file__icon"
+      sprite="doctype"
+      icon={fileType}
+      title={fileType}
+    />
+  );
+};
+
+FileMedia.defaultProps = {
+  image: null,
+};
+
+FileMedia.propTypes = {
+  fileType: PropTypes.string.isRequired,
+  image: PropTypes.shape({
+    alt: PropTypes.string,
+    src: PropTypes.string,
+  }),
+  isLoading: PropTypes.bool.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default FileMedia;
---
+++ src/components/File/__tests__/FileMedia.spec.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Icon } from '../../../';
+import FileMedia from '../FileMedia';
+
+const getComponent = (props = {}) => shallow(
+  <FileMedia
+    fileType="foo"
+    isLoading={false}
+    title="bar"
+    image={null}
+    {...props}
+  />
+);
+
+describe('<FileMedia />', () => {
+  it('renders an icon if no image is present', () => {
+    const mounted = getComponent();
+    const $icon = mounted.find(Icon);
+
+    expect($icon.exists()).toBeTruthy();
+    expect($icon.props()).toMatchObject({
+      icon: 'foo',
+      title: 'foo',
+    });
+  });
+
+  it('renders a loading indicator if isLoading is set', () => {
+    const mounted = getComponent({ isLoading: true });
+    const $icon = mounted.find(Icon);
+
+    expect($icon.exists()).toBeTruthy();
+    expect($icon.props()).toMatchObject({
+      icon: 'image',
+      svgClassName: 'slds-file__loading-icon',
+      title: 'bar',
+    });
+  });
+
+  it('renders an image if passed', () => {
+    const img = { src: 'bar.jpg' };
+    const mounted = getComponent({ image: img });
+    const $img = mounted.find('img');
+
+    expect($img.exists()).toBeTruthy();
+    expect($img.props()).toMatchObject({
+      src: 'bar.jpg',
+      alt: 'bar',
+    });
+  });
+
+  it('allows to set an alt on the image', () => {
+    const img = { src: 'bar.jpg', alt: 'baz' };
+    const mounted = getComponent({ image: img });
+    const $img = mounted.find('img');
+
+    expect($img.exists()).toBeTruthy();
+    expect($img.props()).toMatchObject({
+      src: 'bar.jpg',
+      alt: 'baz',
+    });
+  });
+});
