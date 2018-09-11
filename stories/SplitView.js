a12baf7e66a91a5b21b9695473b32e02a46ca708
Denis Rodin <quadreex@gmail.com>
2018-09-11 13:23:43 +0200

    extend storybook

--- stories/SplitView.js
+++ stories/SplitView.js
@@ -1,7 +1,10 @@
 
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Box, Column, SplitView } from '../src';
+import { boolean } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+
+import { Box, Column, ControlledSplitView, SplitView } from '../src';
 
 const stories = storiesOf('SplitView', module);
 
@@ -35,5 +38,23 @@ stories
         <Box theme="shade">Master view</Box>
         </Column>
       )}
+    />
+  ))
+  .add('Controlled', () => (
+    <ControlledSplitView
+      id="split-view-ctrl"
+      detail={(
+        <Column className="slds-theme_default slds-grid_vertical-align-center">
+          <Box theme="shade">Details view</Box>
+        </Column>
+      )}
+      master={(
+        <Column className="slds-theme_default">
+        <Box theme="shade">Master view</Box>
+        </Column>
+      )}
+      isOpen={boolean('isOpen', true)}
+      onOpen={action('Opened')}
+      onClose={action('Closed')}
     />
   ));
