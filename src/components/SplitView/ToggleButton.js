f452df8e4c94dc3d9d65f09d7aa876c0be140eb6
Denis Rodin <quadreex@gmail.com>
2018-09-20 15:02:42 +0200

    Fix SplitView toggle button (#351)

--- src/components/SplitView/ToggleButton.js
+++ src/components/SplitView/ToggleButton.js
@@ -4,6 +4,7 @@ import cx from 'classnames';
 import { IconSVG } from '../Icon';
 
 export const TOGGLE_BUTTON_WIDTH = '0.75rem';
+const NO_SHADOW = { boxShadow: 'none' };
 
 export const ToggleButton = ({
   ariaControls,
@@ -26,12 +27,16 @@ export const ToggleButton = ({
       aria-expanded={isOpen}
       className={buttonClassName}
       onClick={onClick}
+      style={NO_SHADOW}
       title={assistiveText}
       type="button"
     >
       <IconSVG
         className="slds-button__icon slds-button__icon_x-small"
-        icon={isOpen ? 'left' : 'right'}
+        /* SLDS applies rotate tranform to this icon,
+           so we don't need to change icon itself
+         */
+        icon="left"
         isButton
         sprite="utility"
       />
