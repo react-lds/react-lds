e02ec37cef0bed8903b574b218ea4c460e247c11
Felix Spöttel <hi@felics.me>
2018-10-16 13:15:56 +0200

    Allow passing figureRenderers to MediaObject

--- src/components/MediaObject/MediaObject.js
+++ src/components/MediaObject/MediaObject.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
-const renderFigure = (figure, ...classes) => (
-  <div className={cx('slds-media__figure', ...classes)}>{figure}</div>
+const renderFigure = (figure, classes) => (
+  <div className={cx(...classes)}>{figure}</div>
 );
 
 const MediaObject = (props) => {
@@ -14,13 +14,19 @@ const MediaObject = (props) => {
     customTag,
     figureLeft,
     figureRight,
+    renderFigureLeft,
+    renderFigureRight,
     responsive,
     size,
     title,
     truncate,
-    ...rest } = props;
+    ...rest
+  } = props;
 
   const Tag = customTag || 'div';
+
+  const figureLeftRenderer = renderFigureLeft || renderFigure;
+  const figureRightRenderer = renderFigureRight || renderFigure;
 
   return (
     <Tag
@@ -33,14 +39,14 @@ const MediaObject = (props) => {
         className
       )}
     >
-      {figureLeft && renderFigure(figureLeft)}
+      {figureLeft && figureLeftRenderer(figureLeft, ['slds-media__figure'])}
       <div
         className={cx('slds-media__body', { 'slds-truncate': truncate })}
         title={title}
       >
         {children}
       </div>
-      {figureRight && renderFigure(figureRight, 'slds-media__figure_reverse')}
+      {figureRight && figureRightRenderer(figureRight, ['slds-media__figure', 'slds-media__figure_reverse'])}
     </Tag>
   );
 };
@@ -52,6 +58,8 @@ MediaObject.defaultProps = {
   customTag: null,
   figureLeft: null,
   figureRight: null,
+  renderFigureLeft: null,
+  renderFigureRight: null,
   responsive: false,
   size: null,
   truncate: false,
@@ -83,6 +91,8 @@ MediaObject.propTypes = {
    * renders a figure on the right side of the media object
    */
   figureRight: PropTypes.node,
+  renderFigureLeft: PropTypes.func,
+  renderFigureRight: PropTypes.func,
   /**
    * renders a responsive variant of the MediaObject
    */
