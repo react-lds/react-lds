08e5067010d9cd5a5e041ec62ba5abb1fe5314db
Felix Spöttel <fspoettel@users.noreply.github.com>
2018-10-30 14:22:41 +0100

    Add onInputLabel to Combobox, add alwaysDisplay option to Combobox items, allow empty renders (#365)
    * Add onInputLabelClick to ComboboxDropdown
    
    * Add back alwaysDisplay option to icons in ComboboxDropdownItems
    
    * Allow ComboboxDropdown to be empty
    * Return early if ComboboxDropdownList does not have children
    * Allow React elements as children for ComboboxDropdownItems

--- src/components/Combobox/ComboboxDropdownList.js
+++ src/components/Combobox/ComboboxDropdownList.js
@@ -9,6 +9,8 @@ const ComboboxDropdownList = (props) => {
     id
   } = props;
 
+  if (!children) return null;
+
   const listClasses = [
     { [`slds-dropdown_length-with-icon-${height}`]: height },
     'slds-dropdown',
