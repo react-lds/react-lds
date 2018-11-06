6330155759e74d974db5965757e4ef409f071fa0
Denis Rodin <quadreex@gmail.com>
2018-11-06 17:17:56 +0100

    Fix horizontal scroll for DataTable with fixed header (#368)

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -16,7 +16,7 @@ const Table = (props) => {
   const sldsClasses = [
     'slds-table',
     'slds-table_cell-buffer',
-    { 'slds-table--header-fixed': fixedHeader },
+    { 'header-fixed': fixedHeader },
     className,
     applyDecorators(flavor, 'table'),
     applyDecorators(variation),
