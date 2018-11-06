a515698e3e1c88d7d782331f0866e1b31a8a8f9a
Felix Spöttel <hi@felics.me>
2018-11-06 18:06:04 +0100

    Add ButtonGroup row option

--- src/components/Button/ButtonGroups/ButtonGroup.js
+++ src/components/Button/ButtonGroups/ButtonGroup.js
@@ -2,34 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
-const wrapLi = (el, i) => <li key={i}>{el}</li>;
-
 const ButtonGroup = (props) => {
   const {
     children,
     className,
     list,
+    row,
     ...rest
   } = props;
 
+  const wrapLi = (el, i) => (
+    <li className={row ? 'slds-button-group-item' : null} key={i}>{el}</li>
+  );
+
+  const isDivGroup = !list && !row;
+
   const sldsClasses = cx(
-    list ? 'slds-button-group-list' : 'slds-button-group',
+    { 'slds-button-group': isDivGroup },
+    { 'slds-button-group-list': list },
+    { 'slds-button-group-row': row },
     className
   );
 
-  const GroupEl = list ? 'ul' : 'div';
-  const childEls = list ? React.Children.map(children, wrapLi) : children;
+  const ButtonGroupEl = isDivGroup ? 'div' : 'ul';
+  const childEls = !isDivGroup ? React.Children.map(children, wrapLi) : children;
 
   return (
-    <GroupEl {...rest} className={cx(sldsClasses)} role="group">
+    <ButtonGroupEl
+      {...rest}
+      className={cx(sldsClasses)}
+      role={isDivGroup ? 'group' : null}
+    >
       {childEls}
-    </GroupEl>
+    </ButtonGroupEl>
   );
 };
 
 ButtonGroup.defaultProps = {
   className: null,
   list: false,
+  row: false,
 };
 
 ButtonGroup.propTypes = {
@@ -45,6 +57,10 @@ ButtonGroup.propTypes = {
    * Renders the button group as a list
    */
   list: PropTypes.bool,
+  /**
+   * Renders the button group as a row of separate buttons
+   */
+  row: PropTypes.bool,
 };
 
 export default ButtonGroup;
