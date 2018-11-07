5ff9136a039670d9c76b4a967831d6904dd08420
Felix Spöttel <hi@felics.me>
2018-11-07 14:52:39 +0100

    Add UserAvatar, add EntityAvatar, deprecate Avatar

--- stories/Image.js
+++ stories/Image.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { boolean, text } from '@storybook/addon-knobs';
-import { Avatar } from '../src';
+import { boolean, text, select } from '@storybook/addon-knobs';
+import { Avatar, UserAvatar, EntityAvatar } from '../src';
 import { getSizes } from './utils/helpers';
 
 const stories = storiesOf('Image', module);
 
 stories
-  .add('Default', () => (
+  .add('User Avatar', () => (
+    <UserAvatar
+      imageSrc={text('Src', 'assets/images/avatar2.jpg')}
+      fallbackType={select('Fallback (remove imageSrc to see)', [
+        'profile-image',
+        'group-image',
+        'icon',
+        'initials',
+        'initials-inverse'
+      ], 'icon')}
+      name={text('User Name', 'John Doe')}
+      round={boolean('Round?', false)}
+      size={getSizes()}
+      title={text('Title', 'Avatar of record owner')}
+    />
+  ))
+  .add('Entity Avatar', () => (
+    <EntityAvatar
+      displayType={select('Fallback Type (remove imageSrc to see effect', ['icon', 'initials'], 'icon')}
+      icon={{
+        sprite: 'standard',
+        icon: 'account',
+      }}
+      name={text('Entity Name', 'Record Type')}
+      size={getSizes()}
+      title={text('Title', 'Icon of Record Type')}
+    />
+  ))
+  .add('Avatar (Deprecated)', () => (
     <Avatar
       alt={text('Alt', 'alt text')}
       src={text('Src', 'assets/images/avatar2.jpg')}
