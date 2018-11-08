bc373c6ca064e0bdc88e1737eab013dd6ba164d0
Bruno <bruno.pejac@gmail.com>
2018-11-08 14:30:50 +0100

    Add inline Spinner, Spinner container convenience prop

--- stories/Spinner.js
+++ stories/Spinner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { boolean, select } from '@storybook/addon-knobs';
-import { Spinner, SpinnerContainer } from '../src';
+import { Button, Spinner, SpinnerContainer } from '../src';
 import { getSizes } from './utils/helpers';
 import { SpinnerDecorator } from './utils/decorators';
 
@@ -14,6 +14,7 @@ stories
       delayed={boolean('Delayed', true)}
       flavor={select('Flavor', ['', 'brand', 'inverse'], '') || undefined}
       size={getSizes()}
+      inline={boolean('Inline', false)}
     />
   ))
   .add('With Container', () => (
@@ -23,4 +24,17 @@ stories
         size={getSizes()}
       />
     </SpinnerContainer>
+  ))
+  .add('With Container convenience prop', () => (
+    <Spinner container />
+  ))
+  .add('Inline Spinner', () => (
+    <div>
+      <span>Lorem Ipsum</span>
+      <Spinner
+        size="x-small"
+        inline
+      />
+      <Button>foo bar</Button>
+    </div>
   ));
