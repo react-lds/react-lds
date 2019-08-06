1496eff21e9a961bdedeb8f543b8c5f0cefdd300
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2019-08-06 07:17:52 +0000

    8.6.0 (#446)
    * Deploy docs only on tags
    * Update changelog & bump version

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -2,27 +2,26 @@
 
 set -o errexit -o nounset
 
-# Do nothing if the commit was not pushed to master
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$TRAVIS_BRANCH" != "master" ]
+if [ -n "$TRAVIS_TAG" ]
 then
-  echo "This commit was either a pull request or made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  #  Set Travis as commiter git user
+  git config --global user.email "propertybase-ci@propertybase.com"
+  git config --global user.name "Propertybase CI"
+
+  # Build docs
+  yarn build:storybook
+
+  # Initialize an empty repository, fetch upstream and reset it
+  rev=$(git rev-parse --short HEAD)
+  cd ./react-lds
+  git init
+
+  # Add all changes in push them to gh-pages
+  touch .
+  git add -A .
+  git commit --quiet --message "rebuild pages at ${rev}"
+  git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+else
+  echo "Not a tagged commit, skipping deploy..."
   exit 0
 fi
-
-#  Set Travis as commiter git user
-git config --global user.email "propertybase-ci@propertybase.com"
-git config --global user.name "Propertybase CI"
-
-# Build docs
-yarn build:storybook
-
-# Initialize an empty repository, fetch upstream and reset it
-rev=$(git rev-parse --short HEAD)
-cd ./react-lds
-git init
-
-# Add all changes in push them to gh-pages
-touch .
-git add -A .
-git commit --quiet --message "rebuild pages at ${rev}"
-git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
